206627728da84c4119d5daf93bd7990056297fed
Mike Schlachter <mike@kangopak.co.za>
2025-08-01 23:34:03 +0200

    Replace flat ESLint config with classic config to fix root warning

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+﻿module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  settings: {
+    react: { version: 'detect' }
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': ['warn'], // soften for now
+    'react/react-in-jsx-scope': 'off'
+  }
+};
